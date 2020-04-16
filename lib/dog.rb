7c3357055e820e1b8177cfef258636db33bf9620
Dominique De León <dominique.a.deleon@gmail.com>
2020-04-16 18:38:54 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
 # Add your code here
+require "pry"
+
+class Dog
+  @@all = []
+  
+  def initialize(name)
+  
+     @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    self.all.clear
+    # @@all.clear
+    # @@all = []
+  end
+  
+  ## go through that @@all array 
+  ## how do I go through an array?
+  ## how do I access individual dogs in an array
+  ## how do I print the dog's name?
+  
+  def self.print_all
+    self.all.each do |dog|
+      binding.pry
+    end
+  end
+end
+
+# d = Dog.new("name")
+# # d.all
+# Doggo.all
